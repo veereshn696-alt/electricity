07ce09f1856c451137e4267e9b51724799315d45
veereshn696-alt <veereshn696@gmail.com>
2025-11-27 16:02:21 +0530

    Create electricity.py

---
+++ electricity.py
@@ -0,0 +1,12 @@
+import sys
+if len(sys.argv)!=1:
+  print("usage : python script.py <unit>")
+  unit=10
+else :
+  script_name=sys.argv[0]
+  unit=sys.argv[1]
+perunit=5
+bill=unit*perunit
+print("unit =",unit)
+print("perunit =",perunit)
+print("bill =",bill)
